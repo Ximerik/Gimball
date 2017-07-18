15b1b1fd7af7ef7b59482e9f0d6738d1989badc7
Maksim M <ximerik@ex.ua>
2017-07-18 22:26:05 +0300

    Update control_unit.c

--- control_unit.c
+++ control_unit.c
@@ -1,5 +1,16 @@
 #include <settings.h>
-int* state(struct ring_bufer* angle,
-           struct ring_bufer* angular_speed){
-  static uint1_t state[2];
-  
+
+#define ANGLE 0
+#define ANGULAR_SPEED 1
+
+uint16_t* state(void)
+{      
+  static uint16_t state[2]; // state[0] is for absolute angle
+                            // state[1] is for angular speed
+  state[ANGLE]         = average(angle);
+  state[ANGULAR_SPEED] = average(angular_speed);
+  return state;
+}
+uint16_t average (struct ring_bufer *this_bufer){
+  uint8_t count = 0;
+  while ((count++)<SIZE-1)
