a8c4b64e32903bc2c170773f98ee12febedee2cd
Maksim M <ximerik@ex.ua>
2017-07-18 22:58:03 +0300

    Update control_unit.c

--- control_unit.c
+++ control_unit.c
@@ -1,4 +1,5 @@
 #include <settings.h>
+#include <ctype.h>
 
 #define ANGLE 0
 #define ANGULAR_SPEED 1
@@ -11,6 +12,15 @@ uint16_t* state(void)
   state[ANGULAR_SPEED] = average(angular_speed);
   return state;
 }
+
 uint16_t average (struct ring_bufer *this_bufer){
   uint8_t count = 0;
-  while ((count++)<SIZE-1)
+  uint16_t sum;
+  while ((count++)<SIZE){
+    sum+=memory[(*this_bufer -> read_count)++];
+  return sum/SIZE;
+}
+
+uint16_t position_error( (uint16_t) current_state, (uint16) set_state){
+  return (set_state - current_state);
+}
