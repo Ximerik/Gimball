e6fcbfde5d1bbe8c21a087ae5e96eda1f3376c90
Maksim M <ximerik@ex.ua>
2017-07-19 12:40:49 +0300

    Update fifo.c

--- fifo.c
+++ fifo.c
@@ -1,46 +1,63 @@
-or#include "settings.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <inttypes.h>
+
+#define SIZE 5
 
 struct ring_bufer{
     uint16_t memory[SIZE];
-    int write_count,
-        read_count;
-}angle, angular_speed;
+    uint8_t write_count,
+            read_count;
+};
+/**< Доработать проверку на полный и пустой буфер.
+     Вставить проверку в запись и чтение*/
 int is_full(struct ring_bufer *this_bufer){
-    if (((this_bufer->write_count) - (this_bufer->read_count)) != 0)
+    if ((( this_bufer->write_count) - (this_bufer->read_count)) != 0
+        || this_bufer->write_count == 0 && this_bufer->read_count == 0)
         return 0;
     else
         return 1;
 }
-int is_empty(struct struct ring_bufer *this_bufer){
-    if ((((*this_bufer->write_count) - (*this_bufer->read_count)) == 0)
+int is_empty(struct ring_bufer *this_bufer){
+    if (((this_bufer->write_count) - (this_bufer->read_count)) == 0)
         return 1;
     else
         return 0;
 }
 void write(char data, struct ring_bufer *this_bufer){
-    while(isfull() != 1){
-        this_bufer -> memory[(*this_bufer -> write_count)++] = data;
-        if (*this_bufer -> write_count > SIZE-1)
-            *this_bufer -> write_count = 0;
-    }
+    while (is_full(this_bufer)!=1 && data != '\0')
+            this_bufer -> memory[(this_bufer -> write_count)++] = data;
+    if (this_bufer -> write_count > SIZE-1)
+    this_bufer -> write_count = 0;
 }
 void read(char* data, struct ring_bufer *this_bufer){
-    while(isempty() != 1){
-        *data++ = this_bufer -> memory[(*this_bufer -> read_count)++];
-        if (*this_bufer -> read_count > SIZE-1)
-            *this_bufer -> read_count = 0;
-    }
+        *data++ = this_bufer -> memory[(this_bufer -> read_count)++];
+        if (this_bufer -> read_count > SIZE-1)
+            this_bufer -> read_count = 0;
 }
 void clear_bufer(struct ring_bufer *this_bufer){
-    *this_bufer -> write_count = 0;
-    *this_bufer -> read_count = 0;
+    this_bufer -> write_count = 0;
+    this_bufer -> read_count = 0;
 }
 uint16_t getdata_pid(struct ring_bufer *this_bufer){
-    while(isempty() != 1){
-        return this_bufer -> memory[(*this_bufer -> read_count)++];
-        if (*this_bufer -> read_count > SIZE-1)
-            *this_bufer -> read_count = 0;
+    while(is_empty(this_bufer) != 1){
+        return this_bufer -> memory[(this_bufer -> read_count)++];
+        if (this_bufer -> read_count > SIZE-1)
+            this_bufer -> read_count = 0;
     }
-    return COMAND; 
-    // if bufer is empty - foo return COMAND value for zero PID output
+    return 1;
+    /**< if bufer is empty - foo return COMAND value for zero PID output */
+}
+
+/*
+* GLOBAL
+*/
+struct ring_bufer bufer;
+
+int main()
+{
+    write('a', &bufer);
+    write('a', &bufer);
+    return 0;
 }
